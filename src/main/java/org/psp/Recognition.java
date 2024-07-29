9b416cc26f3dc9faac41354d96385e08974e15cb
psp <syperpsp@yandex.ru>
2024-07-29 21:01:57 +0400

    Logging was added

--- src/main/java/org/psp/Recognition.java
+++ src/main/java/org/psp/Recognition.java
@@ -1,17 +1,23 @@
 package org.psp;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.log4j.PropertyConfigurator;
+
 public class Recognition {
+    private final static Logger LOG = LoggerFactory.getLogger(Recognition.class.getName());
+    private static final String LOG_CONFIG_FILE = System.getProperty("user.dir") + File.separator + "rec_log.properties";
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        PropertyConfigurator.configure(LOG_CONFIG_FILE);
+        
+        LOG.debug("Recognition start");
         System.out.printf("Hello and welcome!\n");
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+        LOG.debug("Recognition end");
     }
 }
